68367ba7d663cc71904b7088b501b4f63947d3b1
melaniepeu <49374642+melaniepeu@users.noreply.github.com>
2019-06-26 11:14:51 +0200

    Creation meteo-component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <meteo/>
+  </div>
+</template>
+
+<script>
+import meteo from './components/Meteo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    meteo
+  }
+}
+</script>
---
+++ src/components/Meteo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Météo</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "meteo"
+}
+</script>
